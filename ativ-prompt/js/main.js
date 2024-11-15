dfc86ecc8c2a92c03aec94fdd3a4d8e911852709
karolayneL <karolaynelira3@gmail.com>
2024-11-15 18:05:04 -0300

    Add files via upload

---
+++ ativ-prompt/js/main.js
@@ -0,0 +1,10 @@
+let cor = prompt( "escreva nome da cor vermelho em inglês para aparecer")
+
+document.querySelector(".body")
+
+if (cor == "red") {
+    document.body.style.backgroundColor = "red";
+}
+else{
+    alert("errou, digite red")
+}
